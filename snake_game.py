14cb4490195ce89d1128025625255f527e09943a
novyoo <navya13m@gmail.com>
2026-04-25 19:35:10 +0530

    Set up the screen

---
+++ snake_game.py
@@ -0,0 +1,13 @@
+#snake game in python
+
+import turtle
+
+#setting up the screen / the window that pops up
+win = turtle.Screen()
+win.title("S N A K E  G A M E")
+win.bgcolor("LightBlue1")
+win.setup(width = 700, height = 700)
+win.tracer(0) #turns off animations on screen
+
+
+win.mainloop()
