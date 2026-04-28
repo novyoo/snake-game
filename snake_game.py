4983f26bf99dda4ebf4bedf2536bfb6f69ffe5bf
novyoo <navya13m@gmail.com>
2026-04-28 13:35:27 +0530

    added custom snake head (used procreate)

--- snake_game.py
+++ snake_game.py
@@ -3,11 +3,24 @@
 import turtle
 
 #setting up the screen / the window that pops up
-win = turtle.Screen()
-win.title("S N A K E  G A M E")
-win.bgcolor("LightBlue1")
-win.setup(width = 700, height = 700)
-win.tracer(0) #turns off animations on screen
+screen = turtle.Screen()
+screen.title("S N A K E  G A M E")
+screen.bgcolor("LightBlue1")
+screen.setup(width = 700, height = 700)
+screen.tracer(0) #turns off animations on screen
 
+# Register your PNG image
+screen.addshape("snake_head.gif")
 
-win.mainloop()
+head = turtle.Turtle()
+head.speed(0) #fastest animation speed (there's like no slowdown)
+head.shape("snake_head.gif")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
+#Main game loop, this repeats over and over again
+while True:
+    screen.update()
+
+screen.mainloop()
