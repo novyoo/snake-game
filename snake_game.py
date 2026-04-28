83252797557fc5c2908c4d6e8095386a88303ba3
novyoo <navya13m@gmail.com>
2026-04-28 14:56:07 +0530

    i added snake food that i made on procreate

--- snake_game.py
+++ snake_game.py
@@ -2,14 +2,15 @@
 
 import turtle
 import time
+import random
 
-delay = 0.2
+delay = 0.15
 
 # Setting up the screen / the window that pops up
 screen = turtle.Screen()
 screen.title("S N A K E  G A M E")
 screen.bgcolor("LightBlue1")
-screen.setup(width = 700, height = 700)
+screen.setup(width = 600, height = 600)
 screen.tracer(0) #turns off animations on screen
 
 # Register your PNG image
@@ -17,13 +18,23 @@ screen.addshape("snake_up.gif") #i made sum random on procreate :333
 screen.addshape("snake_down.gif")
 screen.addshape("snake_right.gif")
 screen.addshape("snake_left.gif")
+screen.addshape("food.gif")
 
+# Head
 head = turtle.Turtle()
 head.speed(0) #fastest animation speed (there's like no slowdown)
 head.shape("snake_up.gif")
 head.penup()
 head.goto(0,0)
 head.direction = "stop"
+
+# Food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("food.gif")
+food.penup()
+food.goto(0,100)
+
 
 # Functions for direction
 def moves():
@@ -77,6 +88,13 @@ screen.onkeypress(going_left, "Left")
 # Main game loop, this repeats over and over again
 while True:
     screen.update()
+
+    if head.distance(food) < 20:
+        
+        # Move food to random spot
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x, y)
 
     moves()
 
